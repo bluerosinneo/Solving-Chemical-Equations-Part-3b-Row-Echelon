f43a3ef283008e9a57a274cf8a1848b004fc961e
bluersinneo <cramergrimes@gmail.com>
2017-12-17 19:59:59 -0500

    Comments
    Comments

--- rowEchelonDriver.py
+++ rowEchelonDriver.py
@@ -1,3 +1,9 @@
+# This code follows the following post.
+# http://cramerexplainsmath.com/2017/12/18/solving-chemical-equations-part-3b-row-echelon/
+# @desc A basic driver that of rowEchelon.  A detailed description of each
+# method can be found in the post
+# @author Cramer Grimes cramergrimes@gmail.com
+
 from rowEchelon import rowEchelon
 
 A = [[1,0,-3,0],
